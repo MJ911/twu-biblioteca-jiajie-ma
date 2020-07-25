7410bce433ae5d8eee80dd6f547915c073741a8d
jiajie <jiajie.ma1@thoguhtworks.com>
2020-07-26 00:46:46 +0800

    [jaijie][1.3] Viewed author and publication year on all books.

--- TWU_Biblioteca/src/main/java/com/twu/biblioteca/console/Console.java
+++ TWU_Biblioteca/src/main/java/com/twu/biblioteca/console/Console.java
@@ -21,8 +21,8 @@ public class Console {
         printer.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
     public void listBooks() {
-        final String ENTRY_FORMAT = "-%-16s | %16s\n";
-        printer.println("= Listing all books in Biblioteca:");
+        final String ENTRY_FORMAT = "%-16s | %16s\n";
+        printer.println("Listing all the books message in Biblioteca:");
         printer.printf(ENTRY_FORMAT, "-AUTHOR-", "-PUBLISH YEAR-");
         libraryBooks.getAllBooks().forEach(book -> System.out.printf(ENTRY_FORMAT, book.getAuthor(), book.getPublishYear()));
     }
