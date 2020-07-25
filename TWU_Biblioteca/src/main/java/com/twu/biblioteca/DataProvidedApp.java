88e431e3695a817c67dd3ba9991767c9da711586
jiajie <jiajie.ma1@thoguhtworks.com>
2020-07-25 23:30:56 +0800

    [jaijie][1.2.2] Added the DataProvidedApp file with provideBookLibrary method which return pre-existing list of Book by TestLibrary.

---
+++ TWU_Biblioteca/src/main/java/com/twu/biblioteca/DataProvidedApp.java
@@ -0,0 +1,17 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.library.Book;
+import com.twu.biblioteca.library.TestLibrary;
+
+import java.util.Arrays;
+
+public final class DataProvidedApp {
+    private DataProvidedApp(){}
+
+    public TestLibrary provideBookLibrary() {
+        return new TestLibrary(Arrays.asList(
+                new Book("Dimtree", 1992),
+                new Book("Nick M", 2003),
+                new Book("Mint", 2005)));
+    }
+}
