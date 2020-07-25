1a9dbcfd300a6251d76076c1bd260fcf440b1d6e
jiajie <jiajie.ma1@thoguhtworks.com>
2020-07-25 23:07:24 +0800

    [jaijie][1.2.1] Added the Book class file.

---
+++ TWU_Biblioteca/src/main/java/com/twu/biblioteca/library/Book.java
@@ -0,0 +1,26 @@
+package com.twu.biblioteca.library;
+
+public class Book {
+    private String author;
+    private int publishYear;
+
+    public Book(String author, int publishYear) {
+        this.author = author;
+        this.publishYear = publishYear;
+    }
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+}
