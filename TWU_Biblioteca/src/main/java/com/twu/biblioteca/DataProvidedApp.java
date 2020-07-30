e058054aa80a1a8887999b6c35e8ecd165c45aee
jiajie <jiajie.ma1@thoguhtworks.com>
2020-07-31 01:02:59 +0800

    [jiajie][2.1.5] add the pre-existing movies Library method called TestLibraryMovies in the DataProvidedApp.

--- TWU_Biblioteca/src/main/java/com/twu/biblioteca/DataProvidedApp.java
+++ TWU_Biblioteca/src/main/java/com/twu/biblioteca/DataProvidedApp.java
@@ -1,7 +1,9 @@
 package com.twu.biblioteca;
 
 import com.twu.biblioteca.library.Book;
+import com.twu.biblioteca.library.Movie;
 import com.twu.biblioteca.library.TestLibraryBooks;
+import com.twu.biblioteca.library.TestLibraryMovies;
 
 import java.util.Arrays;
 
@@ -14,5 +16,13 @@ public final class DataProvidedApp {
                 new Book("1","Dimtree", 1992, true),
                 new Book("2","Nick M", 2003,true),
                 new Book("3","Mint", 2005,true)));
+    }
+
+    public static TestLibraryMovies provideMovieLibrary() {
+        return new TestLibraryMovies(Arrays.asList(
+                new Movie("1","X MAN", "2001", "Eric", "7", true),
+                new Movie("2","X MAN 2", "2004", "Eric & Nick", "9", true),
+                new Movie("3","X MAN 3", "2010", "Nick", "10", true)
+        ));
     }
 }
